9da41d8e018f4b2e6ff49d3edb07f35643dd33fd
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-09-22 23:49:30 +0530

    code clean up

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -19,20 +19,3 @@ const deploy = async () => {
   provider.engine.stop();
 };
 deploy();
-// //
-// const web3 = new Web3(provider);
-
-// const deploy = async () => {
-//   const accounts = await web3.eth.getAccounts();
-
-//   console.log('Attempting to deploy from account', accounts[0]);
-
-//   const result = await new web3.eth.Contract(abi)
-//     .deploy({ data: evm.bytecode.object })
-//     .send({ gas: '1000000', from: accounts[0] });
-
-//   console.log(JSON.stringify(abi));
-//   console.log('Contract deployed to', result.options.address);
-//   provider.engine.stop();
-// };
-// deploy();
